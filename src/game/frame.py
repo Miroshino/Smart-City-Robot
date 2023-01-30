f6b38db59cf0fbc56bb76191a51375cd26c8468e
Whiletruend <bongage.benjamin@gmail.com>
2023-01-30 16:00:42 +0100

    (30/01/2023): Setup first class "frame".

---
+++ src/game/frame.py
@@ -0,0 +1,24 @@
+# Imports
+import tkinter as tk
+
+# Frame class
+class Frame:
+    # Construct (init)
+    def __init__(self, title, size):
+        self.title: str = title
+        self.size: str = size
+
+    # Show func
+    def show(self):
+        # Frame
+        Game_Frame: tk = tk.Tk()
+        Game_Frame.geometry(self.size)
+        Game_Frame.title(self.title)
+        Game_Frame.resizable(False, False)
+
+        # Board
+        Game_Board: tk = tk.Frame(Game_Frame, width=350, height=350)
+
+        # Show the frame
+        Game_Board.pack(padx=10, pady=25)
+        Game_Frame.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-# Import(s)
+# Imports
+from src.game.frame import Frame
+
+# Game_Frame
+game_frame: Frame = Frame("Tic'T-AI'Toe", "400x400")
+game_frame.show()
